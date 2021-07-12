a1d30f6e77196ef0d00030f12dbcc5277b6b9050
Sushmita Sen <sushmitasen94@gmail.com>
2021-07-12 12:19:01 +0530

    First commit!

---
+++ portfolio/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from portfolio.models import Project
+from blog.models import Blog
+
+admin.site.register(Project)
+admin.site.register(Blog)
+
+
+# Register your models here.
